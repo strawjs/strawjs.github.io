c5989b96450f796655aefc70c03f6dda61d2dee4
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-10 17:46:21 +0900

    Write version pages and the index page

--- middleman/config.rb
+++ middleman/config.rb
@@ -1,55 +1,16 @@
-###
-# Compass
-###
+# directory index (url without .html)
+activate :directory_indexes
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
+# asset dirs
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+
+
+# sprockets bower settings
+set :bower_dir, '../bower_components'
+
+
 
 # Build-specific configuration
 configure :build do
@@ -70,10 +31,16 @@ configure :build do
 end
 
 
+
+# dynamic pages
+
+# index page
+proxy "/index.html", "/version.html", locals: data.android.versions.latest, ignore: true
+
 # version pages
 data.android.versions.each do |version, data|
    proxy "/#{version}.html", "/version.html", locals: data, ignore: true
 end
 
 
-set :bower_dir, '../bower_components'
+
