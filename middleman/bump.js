a7d0ebdd21e154b9f4809e4cefd1344379691bb5
Yoshiya Hinosawa <stibium121@gmail.com>
2013-11-12 13:38:06 +0900

    Version bumper

---
+++ middleman/bump.js
@@ -0,0 +1,198 @@
+#! /usr/bin/env node
+
+var program = require('commander');
+
+
+var Version = (function () {
+    'use strict';
+
+    var exports = function (l, m, s) {
+        this.l = l;
+        this.m = m;
+        this.s = s;
+    };
+
+    var versionPt = exports.prototype;
+
+    versionPt.bumpSmall = function () {
+        this.s ++;
+    };
+
+    versionPt.bumpMedium = function () {
+        this.m ++;
+        this.s = 0;
+    };
+
+    versionPt.bumpLarge = function () {
+        this.l ++;
+        this.m = 0;
+        this.s = 0;
+    };
+
+    versionPt.toString = function () {
+        return 'v' + this.l + '.' + this.m + '.' + this.s;
+    };
+
+    exports.fromString = function (version) {
+        var vars = /v(\d+).(\d+).(\d+)/.exec(version);
+
+        return new exports(vars[1], vars[2], vars[3]);
+    };
+
+    return exports;
+
+}());
+
+var LibVersion = (function () {
+    'use strict';
+
+    var exports = function (name, version, type) {
+        this.name = name;
+        this.version = version;
+        this.type = type;
+    };
+
+    var libVersionPt = exports.prototype;
+
+    libVersionPt.bumpSmall = function () {
+        this.version.bumpSmall();
+    };
+
+    libVersionPt.bumpMedium = function () {
+        this.version.bumpMedium();
+    };
+
+    libVersionPt.bumpLarge = function () {
+        this.version.bumpLarge();
+    };
+
+    libVersionPt.toObject = function () {
+        return {
+            name: this.name,
+            version: this.version.toString(),
+            type: this.type
+        };
+    };
+
+    libVersionPt.hasName = function (name) {
+        return this.name === name;
+    };
+
+    exports.fromObject = function (obj) {
+        return new exports(obj.name, new Version(obj.version), obj.type);
+    };
+
+    return exports;
+
+}());
+
+var BundleVersion = (function () {
+    'use strict';
+
+    var exports = function (platform, name, version, time, libVersions) {
+        this.platform = platform;
+        this.name = name;
+        this.version = version;
+        this.time = time;
+        this.libVersions = libVersions;
+    };
+
+    var bundleVersionPt = exports.prototype = new LibVersion();
+
+    bundleVersionPt.find = function (name) {
+        if (this.hasName(name)) {
+            return this;
+        }
+
+        var found = this.libVersions.filter(function (libVersion) {
+            return libVersion.hasName(name);
+        });
+
+        if (found.length > 0) {
+            return found[0];
+        }
+
+        return null;
+    };
+
+    bundleVersionPt.update = function () {
+        this.time = ~~(new Date() / 1000);
+    };
+
+    bundleVersionPt.getVersionLabel = function () {
+        return this.version.toString();
+    };
+
+    bundleVersionPt.toObject = function () {
+        return {
+            platform: this.platform,
+            name: this.name,
+            version: this.version.toString(),
+            time: this.time,
+            libs: this.libVersions.map(function (lib) { return lib.toObject(); })
+        };
+    };
+
+    exports.createFromLatestForPlatform = function (platform) {
+        return this.fromObject(require('./data/' + platform + '/versions/latest'));
+    };
+
+    exports.fromObject = function (obj) {
+        return new this(
+            obj.platform,
+            obj.name,
+            Version.fromString(obj.version),
+            obj.time,
+            obj.libs.map(function (obj) { return new LibVersion.fromObject(obj); })
+        );
+    };
+
+    return exports;
+
+}());
+
+
+program
+  .version('0.0.1')
+  .usage('-p [platform] [-name <lib name>|-b] [-l|-m|-s]')
+  .option('-p, --platform [platform]', 'target platform [android|ios]')
+  .option('-n, --name [name]', 'target lib [required]')
+  .option('-b, --bundle', 'bump bundle itself')
+  .option('-l, --large', 'large (1.0.0) bump')
+  .option('-m, --medium', 'medium (0.1.0) bump')
+  .option('-s, --small', 'small (0.0.1) bump [default]')
+  .parse(process.argv);
+
+
+try {
+    var bundleVersion = BundleVersion.createFromLatestForPlatform(program.platform);
+
+} catch (e) {
+    console.log('no such platform: ' + program.platform);
+    process.exit(1);
+
+}
+
+if (program.bundle) {
+    lib = bundleVersion;
+
+} else {
+    lib = bundleVersion.find(program.name);
+
+}
+
+if (program.small) {
+    lib.bumpSmall();
+}
+
+if (program.medium) {
+    lib.bumpMedium();
+}
+
+if (program.large) {
+    lib.bumpLarge();
+}
+
+bundleVersion.update();
+
+console.log(lib);
